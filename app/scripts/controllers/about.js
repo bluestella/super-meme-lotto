9ccd7c974dd360a8a1de5b46b3c0c7a4e79f1a06
bluestella <joseph.buluran@gmail.com>
2017-04-18 09:44:50 +0800

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name superMemeLottoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the superMemeLottoApp
+ */
+angular.module('superMemeLottoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
